9e88e90ec703220c7cfd4dbffdf1639897a4f226
fly803 <13521549681>
2018-08-12 12:44:35 +0800

    添加屏幕适配页面和相关库

--- app/src/main/java/com/screenadaptation/test/activity/MainActivity.java
+++ app/src/main/java/com/screenadaptation/test/activity/MainActivity.java
@@ -4,15 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.screenadaptation.base.ScreenAdaptActivity;
 import com.screenadaptation.test.R;
 import com.screenadaptation.utils.ResolutionAdaptationUtils;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ScreenAdaptActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("cg", "onCreate: "+ ResolutionAdaptationUtils.getDensity(this));
     }
 }
